23b72fac627684f72d617a4a33206f97d0b65de9
suryareddy <suryareddy1161@gmail.com>
2022-12-21 19:12:04 +0530

    todo checkbox

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -6,40 +6,77 @@ import { useEffect } from 'react';
 const Todo = () => {
     const [data,SetData]=useState([]);
     const [check,setCheck]= useState(true)
-    const [count,setCount] = useState(0)
+    const [counttrue,setCounttrue] = useState(0)
+    const [countfla,setCountfla] = useState(0)
+ 
    
 
     const url = "https://jsonplaceholder.typicode.com/todos "
-    const checkbox=((e)=>{
-        
-        setCheck({check,[e.target.name]:e.target.value})
-        setCount(count+1)
-    })
+
+
+    const handleClick = (id) => {
+      let _todos = [...data];
+      _todos[id].completed = !_todos[id].completed
+      setCheck(_todos)
+      if(_todos[id].completed == true){
+        setCounttrue(counttrue+1)
+      }
+      else if(_todos[id].completed == false){
+        setCountfla(countfla+1)
+      }
+      
+    }
+ /*  const countt = (()=>{
+   if(data.completed == true){
+    setCounttrue(counttrue+1)
+   }
+   else if (data.completed == false){
+    setCountfla(countfla+1)
+   }
+  }) */
+   
     
     useEffect(()=>{
         axios.get(`${url}`).then((res)=>{
-            console.log(res)
+           /*  console.log(res) */
             SetData(res.data)
             }).catch((err)=>{
                 console.log(err)
-            })
+            }) 
+            
     },[])
+    console.log(data)
   return (
     <div>
         <div className='container' >
             <h1>Todos</h1>
-            <div> count = {count} </div>
+            
+            <div >counttrue = {counttrue}</div>
+            <div>countflase = {countfla}</div>
+           
           {
             data.map((todo,id)=>{
                 return(
                     <div key={id}>
+                      
                         
                     <ul>
                         <li>{todo.title}</li>
-                    
+                        
                     </ul>
-                    <input type={'checkbox'} onClick={checkbox} defaultChecked={todo.completed}></input>
+                    <input
+                    type={"checkbox"}
+                    checked={todo.completed}
+                    onChange={() => handleClick(id)}
+                  />
+                    
+                    {
+                      todo.completed === true ? <div >true</div> : <div >false</div>
+                    }
+                   
+                   
                 </div>
+
                 )
                
             })
